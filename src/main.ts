14cd3eaa3fb39b19171263893c430e9111ee2426
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2025-08-28 03:23:37 +0700

    Modify code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Enable CORS for all origins (allow Flutter web requests)
-  app.enableCors();
+  // Enable CORS using frontend URL from environment
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get('FRONTEND_URL') as string;
+  app.enableCors({ origin: frontendUrl });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => console.error('Nest bootstrap error:', err));
